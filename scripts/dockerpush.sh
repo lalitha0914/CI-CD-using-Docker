28b44824b5587a3967dbbd261d9c84685081f9c7
lalitha0914 <94472907+lalitha0914@users.noreply.github.com>
2023-06-10 22:09:37 +0530

    Update dockerpush.sh

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -1,5 +1,5 @@
 
 #sudo docker login --username=$username --password=$password
 sudo docker logout
-cat ~/password.txt | sudo docker login --username yedukondalu212 --password-stdin
-sudo docker push yedukondalu212/java-app-demo:latest
+cat ~/password.txt | sudo docker login --username lalitha1309 --password-stdin
+sudo docker push lalitha1309/java-app-demo:latest
